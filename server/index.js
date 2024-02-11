5a049c3e47e93ac2782442ee328e0dceac5d1b0f
Maargarita <77836413+Maargarita@users.noreply.github.com>
2024-02-11 15:51:36 +0300

    added tables and relations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.status(200).json({message: 'working!'})
+})
 
 const start = async () => {
     try{
--- server/models/models.js
+++ server/models/models.js
@@ -1,3 +1,61 @@
 const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
+const Role = sequelize.define('role', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+})
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+    login: {type: DataTypes.STRING, unique: true, allowNull: false},
+    passowrd: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Storekeeper = sequelize.define('storekeeper', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+    surname: {type: DataTypes.STRING, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false},
+    patronymic: {type: DataTypes.STRING},
+    phone: {type: DataTypes.STRING, unique: true}
+})
+
+const Warehouse = sequelize.define('warehouse', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+    address: {type: DataTypes.STRING, unique: true, allowNull: false},
+    total_capacity: {type: DataTypes.FLOAT(2), allowNull: false},
+    occupied: {type: DataTypes.FLOAT(2)}
+})
+
+const ProductWarehouse = sequelize.define('product_warehouse', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+})
+
+const Product = sequelize.define('product', {
+    id: {type: DataTypes.STRING, primaryKey: true},
+    article_number: {type: DataTypes.STRING, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false},
+    in_stock: {type: DataTypes.INTEGER}, 
+    warehouse_space: {type: DataTypes.FLOAT(2)}
+})
+
+Role.hasMany(User)
+User.belongsTo(Role)
+
+User.hasOne(Storekeeper)
+Storekeeper.belongsTo(User)
+
+Warehouse.hasMany(Storekeeper)
+Storekeeper.belongsTo(Warehouse)
+
+Warehouse.belongsToMany(Product, {through: ProductWarehouse})
+Product.belongsToMany(Warehouse, {through: ProductWarehouse})
+
+module.exports = {
+    User,
+    Role,
+    Storekeeper,
+    Warehouse,
+    Product,
+    ProductWarehouse
+}
